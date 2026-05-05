6d3380b4e004cd468fbf431e322758a083a56d03
Alexandre Kachler <alexkachler@yahoo.fr>
2026-05-05 14:38:16 +0200

    fix boot hang: disable INT1 during wakeup sequence
    Only enable INT0 (power button) during AllezDebout startup, not INT1
    (IR). With level-triggered INT1, ambient IR noise would fire the IR
    ISR which disabled EIMSK, then relay switching noise triggered the
    Power ISR via INT0, clearing FlagPower and aborting the wakeup.
    INT1 is re-enabled later when entering MainLoop.

--- SoftUGS.asm
+++ SoftUGS.asm
@@ -910,8 +910,8 @@ Wait1:	dec     Work
         
 ; -- On n'oublie pas d'autoriser les interruptions externes --
 
-        ldi     Work,0b00000011                 	; On autorise les interruptions externes INT 0 et INT1 
-        out     EIMSK,Work                      	; (Enable Interrupt Mask)
+        ldi     Work,0b00000001                 	; On autorise seulement INT0 (bouton power) pendant le démarrage
+        out     EIMSK,Work                      	; INT1 (IR) sera activé plus tard dans MainLoop
 
 ; --------------------------------------------------------------------------------------
 ; -- On commence par fixer la luminosité au minimum avant l'alimentation du backlight --
